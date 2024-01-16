6dbba55a8e7a99657e5190080070350ce13dc084
Ana Paula Moura <ana.mcruz@hotmail.com>
2024-01-16 20:06:15 -0300

    first commit

---
+++ src/components/Task/Task.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import './Task.css'
+
+
+function Task() {
+  const[completado,setCompletado] = useState(false)
+  const[tarefa,setTarefa] = useState('')
+
+  useEffect(() => {
+
+    if(completado == true){
+      setTarefa("Tarefa Concluida") 
+    }else{
+      setTarefa(" ")
+    }
+
+  }, [completado]) 
+
+  return (
+    <div className="container">
+        <h1>Tarefa</h1>
+        <h3>Status da Tarefa: {tarefa}</h3>
+        <button onClick={() => {setCompletado(true)}}>Conclua a Tarefa</button>
+     
+    </div>
+  )
+}
+
+export default Task
